8e6079479a0b5c1c7ee0a1b3baf339f3dc9af8cf
karthik <karthikmuppidi7777@gmail.com>
2025-08-09 10:31:19 +0530

    dom object

---
+++ dom/script.js
@@ -0,0 +1,5 @@
+document.body.querySelector('#heroic').innerText='venila css'
+document.getElementById('manali').src='https://i.pinimg.com/originals/84/ed/16/84ed165239f3e217d6ffb72a266d15a4.jpg'
+document.getElementById('space').innerText='react js'
+document.getElementById('end').innerHTML='<b>Angular js</b>'
+document.getElementsByClassName('read').innerHTML
