46221bdabbc63ac46fe3626fb981d503ee429393
karthik <karthikmuppidi7777@gmail.com>
2025-08-12 18:28:01 +0530

    for of loop

--- dialog.js
+++
@@ -1,5 +0,0 @@
-// alert("hello")
-// const p = +prompt("user input ")
-// console.log(p)
-// const m=confirm("can i close now")
-// console.log(m)
--- dom/script.js
+++ dom/script.js
@@ -1,5 +1,10 @@
-document.body.querySelector('#heroic').innerText='venila css'
-document.getElementById('manali').src='https://i.pinimg.com/originals/84/ed/16/84ed165239f3e217d6ffb72a266d15a4.jpg'
-document.getElementById('space').innerText='react js'
-document.getElementById('end').innerHTML='<b>Angular js</b>'
-document.getElementsByClassName('read').innerHTML
+// document.body.querySelector('#heroic').innerText='venila css'
+// document.body.querySelector('#heroic').innerText='venila css'
+// document.body.querySelector('#heroic').innerText='venila css'
+// document.getElementById('manali').src='https://i.pinimg.com/originals/84/ed/16/84ed165239f3e217d6ffb72a266d15a4.jpg'
+// document.getElementById('space').innerText='react js'
+// document.getElementById('end').innerHTML='<b>Angular js</b>'
+// document.getElementsByClassName('read').innerHTML
+var a="karthik";
+console.log(a);
+
